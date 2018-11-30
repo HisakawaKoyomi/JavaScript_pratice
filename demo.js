e2d03daae05cb70060d4d041790a6182bfdbdd0a
HisakawaKoyomi <14yli3@stu.edu.cn>
2018-11-30 16:18:47 +0800

    polyfill_assign

--- demo.js
+++ demo.js
@@ -1,2 +1,12 @@
-let obj = {a: 1};
-console.log(obj);
+var obj = {
+    bar: {
+        value: 2  // bar 是个不可枚举属性。
+    },
+    baz: {
+        value: 3,
+        enumerable: false  // baz 是个自身可枚举属性。
+    }
+};
+
+var copy = Object.assign({}, obj);
+console.log(copy); // { baz: 3 }
